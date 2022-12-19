27d22455020ec7a18c04694369c4ab071f393959
Haicel Balite <116935336+celstice@users.noreply.github.com>
2022-12-20 00:07:29 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+$(document).ready(function () {
+  
+
+const toggleButton = document.getElementsByClassName('menu')[0];
+const navbarLinks = document.getElementsByClassName('links')[0];
+
+toggleButton.addEventListener('click', function(event) {
+	navbarLinks.classList.toggle('active')
+  // banner.class('opacity = 0')
+});
+});
+function menuBar(x){
+  x.classList.toggle("change");
+
+  // $("#navbarContent").css("background-color","transparent");
+  
+}
+
+$(document).ready(function(){
+  $(window).scroll(function(){
+    var scroll = $(window).scrollTop();
+    if (scroll >50){
+      $("#navbar").css("background-color","black");
+    }
+    else {
+      $("#navbar").css("background-color","transparent");
+    }
+  });
+});
+
+// var swiper = new Swiper(".slideBox", {
+//     spaceBetween: 30,
+//     centeredSlides: true,
+//     autoplay: {
+//       delay: 1000,
+//       disableOnInteraction: false,
+//     },
+//     // pagination: {
+//     //   el: ".swiper-pagination",
+//     //   clickable: true,
+//     // },
+//     // navigation: {
+//     //   nextEl: ".swiper-button-next",
+//     //   prevEl: ".swiper-button-prev",
+//     // },
+//   });
