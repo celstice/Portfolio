0687791822e0420c88a63dd291570cf7b84cc0ad
Haicel Balite <116935336+celstice@users.noreply.github.com>
2024-02-29 22:05:46 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ toggleButton.addEventListener('click', function(event) {
   // banner.class('opacity = 0')
 });
 });
+
 function menuBar(x){
   x.classList.toggle("change");
 
@@ -20,7 +21,14 @@ $(document).ready(function(){
   $(window).scroll(function(){
     var scroll = $(window).scrollTop();
     if (scroll >50){
-      $("#navbar").css("background-color","black");
+      $("#navbar").css("background-color","none");
+    //   $("#navbar").css(
+    //     "backdrop-filter", "blur(16px) saturate(180%)",
+    // "-webkit-backdrop-filter", "blur(16px) saturate(180%)",
+    // "background-color","rgba(0, 0, 0, 0.75)",
+    // "border-radius", "12px",
+    // "border", "1px solid rgba(255, 255, 255, 0.125)",
+    //   );
     }
     else {
       $("#navbar").css("background-color","transparent");
